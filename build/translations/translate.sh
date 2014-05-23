f9d82ea63d80fb080b1244016664a58436456c53
Matthew Petroff <matthew@mpetroff.net>
2014-05-23 12:45:59 -0400

    Added Serbian and Spanish translations (thanks Ogi Djuraskovic).

---
+++ build/translations/translate.sh
@@ -0,0 +1,5 @@
+# Serbian
+sed 's/Image /Slika /g' ../slenderbox.js | sed 's/ of / od /g' > slenderbox_sr.js
+
+# Spanish
+sed 's/Image /Imagen /g' ../slenderbox.js | sed 's/ of / de /g' > slenderbox_es.js
